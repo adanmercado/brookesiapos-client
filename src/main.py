74d7e63b321e0067ab41cc862cfaea6eab289431
Adan Mercado <adanmercado.dev@gmail.com>
2022-07-03 01:45:10 -0500

    Added login view

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QCoreApplication
+from PySide6.QtCore import Qt
+
+from ui.loginwidget import LoginWidget
+
+if __name__ == '__main__':
+    QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
+
+    app = QApplication(sys.argv)
+    app.setApplicationName('Brookesia POS')
+    app.setApplicationDisplayName('Brookesia POS')
+    app.setApplicationVersion('1.0.0')
+    app.setOrganizationName('adanmercado')
+    app.setOrganizationDomain('adanmercado.brookesiapos')
+
+    login = LoginWidget()
+    login.show()
+
+    sys.exit(app.exec())
